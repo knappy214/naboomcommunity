aae05fbbcdfcc3aeca25de6955854c83e5e9c261
knappy214 <peter.knapton@gmail.com>
2025-08-30 18:36:58 +0200

    chore: initial  import

---
+++ naboomcommunity/naboomcommunity/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-8gem37f7c)%5q*f#2!mo_6zgra8u=*q5a3h#ln^v*qj&^iq=+j"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
